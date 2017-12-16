c99d3fa790d338ebf4a70b5a73ba7225d1bff7ad
Mr. Hoang Giang Le <hle41@my.centennialcollege.ca>
2017-12-16 16:09:39 -0500

    Json file is readable

--- Comp229-Assign04/Default.aspx.cs
+++ Comp229-Assign04/Default.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+using System.IO;
+
 
 namespace Comp229_Assign04
 {
@@ -11,7 +15,12 @@ namespace Comp229_Assign04
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            string path = HttpContext.Current.Server.MapPath("~/Assets/Assign04.json");
+            string data = File.ReadAllText(path);
+            var collection = JsonConvert.DeserializeObject<List<Models>>(data);
+            allModel.DataSource = collection;
+            allModel.DataBind();
         }
+
     }
 }
